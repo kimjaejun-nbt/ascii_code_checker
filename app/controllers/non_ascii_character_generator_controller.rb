5bcaa87312ac772c4df07954715739227ea82644
Jaejun Kim <kim.jaejun@nbt.com>
2023-06-15 17:40:40 +0900

    json응답으로 내릴 때 경우 (non ascii 문자는 지워가며 비교하는 용도)

--- app/controllers/non_ascii_character_generator_controller.rb
+++ app/controllers/non_ascii_character_generator_controller.rb
@@ -6,4 +6,15 @@ class NonAsciiCharacterGeneratorController < ApplicationController
     #   format.html { render  }
     # end
   end
+
+  def valid_response_by_json
+    render json: {
+      apiSuccess: true,
+      message: 'hello world!',
+      result: {
+        resultCode: "4000",
+        non_ascii: 'é ñ ü ç ß 你 日 ह Ω ¿'
+      }
+    }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
   controller :non_ascii_character_generator do
     get '/check' => :non_ascii_html_char_generate
     post '/check' => :non_ascii_html_char_generate
+
+    get '/valid_request' => :valid_response_by_json
+    post '/valid_request' => :valid_response_by_json
   end
 end
